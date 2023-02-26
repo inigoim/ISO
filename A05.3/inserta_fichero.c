79ca88647298abbb0557dcb2126883a153590042
Iñigo <inigoim2@gmail.com>
2023-02-26 23:14:04 +0100

    Now it works with an existing tar

--- A05.3/inserta_fichero.c
+++ A05.3/inserta_fichero.c
@@ -25,48 +25,77 @@
 #include "s_mytarheader.h"
 
 int BuilTarHeader(char *FileName, struct c_header_gnu_tar * pTarHeader);
+unsigned long WriteFileDataBlocks(int fd_DataFile, int fd_TarFile);
+unsigned long WriteEndTarArchive( int fd_TarFile);
+unsigned long WriteCompleteTarSize( unsigned long TarActualSize,  int fd_TarFile);
+
+int seek_last_header(int fd_mytar, char * header_buffer);
 
 int inserta_fichero(char * f_mytar, char * f_dat)
 {
-    int ret, fd_mytar, fd_dat;
+    int fd_mytar, fd_dat;
     struct c_header_gnu_tar tar_header;
     struct stat stat_mytar;
-    char header_buffer[FILE_HEADER_SIZE];
+    
+    memset(&tar_header, 0, sizeof(tar_header));
+    memset(&stat_mytar, 0, sizeof(stat_mytar));
 
 
     // Open the files
-    if ((fd_dat=open(f_dat, O_RDONLY)) == -1) return E_OPEN1;
-    if ((fd_mytar=open(f_mytar, (O_RDWR | O_CREAT), 0600)) == -1) return E_OPEN2;
+    if ((fd_dat = open(f_dat, O_RDONLY)) == -1)
+        return E_OPEN1;
+    if ((fd_mytar = open(f_mytar, (O_RDWR | O_CREAT), 0600)) == -1)
+        return E_OPEN2;
 
     // Build the data_file header
-    memset(&tar_header, 0, sizeof(tar_header)); // Is this necessary?
-    ret = BuilTarHeader(f_dat, &tar_header);
-    if (ret != HEADER_OK) return E_OPEN1;
+    if (BuilTarHeader(f_dat, &tar_header) != HEADER_OK) return E_OPEN1;
 
-    // Get the size of the tar file
-    memset(&stat_mytar, 0, sizeof(stat_mytar)); // Is this necessary?
+    //TODO: What if the file is empty?
+
+
+    // Write the header and data
+    seek_last_header(fd_mytar);
+    if (write(fd_mytar, &tar_header, FILE_HEADER_SIZE) != FILE_HEADER_SIZE)
+        return E_DESCO;
+    if (WriteFileDataBlocks(fd_dat, fd_mytar) < 0)
+        return E_DESCO;
+
+    // Write the end of the tar file
+    WriteEndTarArchive(fd_mytar);
     fstat(fd_mytar, &stat_mytar);
+    WriteCompleteTarSize(stat_mytar.st_size, fd_mytar);
 
-    //TODO: What if the file doesn't exist?
+    // Close the files
+    close(fd_dat);
+    close(fd_mytar);
 
+    return 0;
+}
+
+
+// Go to the last header of the tar file
+int seek_last_header(int fd_mytar) {
+    char header_buffer[FILE_HEADER_SIZE];
     memset(header_buffer, 0, DATAFILE_BLOCK_SIZE);
-    int file_size;
-    do
+    while (1)
     {
         // Read the header
-        read(fd_mytar, header_buffer, DATAFILE_BLOCK_SIZE);
+        if (read(fd_mytar, header_buffer, DATAFILE_BLOCK_SIZE)
+            != DATAFILE_BLOCK_SIZE)
+            return E_TARFORM;
 
         // Check the size field of the header
         struct c_header_gnu_tar * header = (struct c_header_gnu_tar *) header_buffer;
-        file_size = strtol(header->size, NULL, 8);
-        printf("size field: \"%s\"\n", header->size);
-        printf("Actual size: \"%d\"\n", file_size);
+
+        // TODO: Check if the header is valid
+        int file_size = strtol(header->size, NULL, 8);
+        if (file_size == 0) break;
 
         // Advance to the next header
         int offset = file_size + (DATAFILE_BLOCK_SIZE - (file_size % DATAFILE_BLOCK_SIZE));
         lseek(fd_mytar, offset, SEEK_CUR);
-
-    } while (file_size != 0); //FIXME: This is not the best way to check if it's the last header
-
+    }
     lseek(fd_mytar, -DATAFILE_BLOCK_SIZE, SEEK_CUR);
+
+    return 0;
 }
